f57e220e0112c9d04b001651e643cc7f341d9562
william cole <cole.williams84@yahoo.com>
2025-06-25 20:20:41 +0100

    content updated

--- app/components/ContentSection.tsx
+++ app/components/ContentSection.tsx
@@ -27,7 +27,7 @@ export default async function ContentSection() {
 
                                 <div className="mt-6 space-y-3">
                                     <cite className="block font-medium">John Doe, CEO</cite>
-                                    <img className="h-5 w-fit dark:invert" src="https://html.tailus.io/blocks/customers/nvidia.svg" alt="Nvidia Logo" height="20" width="auto" />
+                                    {/* <Image className="h-5 w-fit dark:invert" src="https://html.tailus.io/blocks/customers/nvidia.svg" alt="Nvidia Logo" height={20} width="auto" /> */}
                                 </div>
                             </blockquote>
                         </div>
