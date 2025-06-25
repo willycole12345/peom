530f4d5d9c350d7a36d964eb5afb064ee9a157fe
william cole <cole.williams84@yahoo.com>
2025-06-25 20:37:58 +0100

    update

--- app/components/ContentSection.tsx
+++ app/components/ContentSection.tsx
@@ -26,7 +26,7 @@ export default async function ContentSection() {
                                 <p>Using TailsUI has been like unlocking a secret design superpower. It's the perfect fusion of simplicity and versatility, enabling us to create UIs that are as stunning as they are user-friendly.</p>
 
                                 <div className="mt-6 space-y-3">
-                                    <cite className="block font-medium">John Doe&#44; CEO</cite>
+                                    <cite className="block font-medium">John Doe CEO</cite>
                                     {/* <Image className="h-5 w-fit dark:invert" src="https://html.tailus.io/blocks/customers/nvidia.svg" alt="Nvidia Logo" height={20} width="auto" /> */}
                                 </div>
                             </blockquote>
