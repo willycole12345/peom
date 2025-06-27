9f8a78a519050fb6c3e74a8c46af049a79145149
william cole <cole.williams84@yahoo.com>
2025-06-27 01:16:33 +0100

    updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import { HeroHeader } from "./components/header"
 import { Card } from '@/components/ui/card'
 import Image from 'next/image'
 import ContentSection from './components/ContentSection'
-import Blogpost from './components/blogpost'
 
 export default function Home() {
   return (
@@ -234,7 +233,7 @@ export default function Home() {
             </div>
           </div>
         </section>
-        <Blogpost />
+        {/* <Blogpost /> */}
       </main>
     </>
   );
